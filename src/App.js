065c9f5c858be740204463df70d4b9de6ef2caf3
shubham <shubh.shubham21@gmail.com>
2022-05-26 19:01:59 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,77 @@
+import { useState } from 'react';
+import { UseContextProvider } from './contexts/StepperContext';
+
 import Stepper from './components/Stepper';
 import StepperControl from './components/StepperControl';
+
 import Account from './components/steps/Account';
+import Details from './components/steps/Details';
+import Final from './components/steps/Final';
+import Payment from './components/steps/Payment';
 
 function App() {
-	const steps = ['Account Information', 'Personal Details', 'Complete'];
+	const [currentStep, setCurrentStep] = useState(1);
 
-	const diplaySteps = (step) => {
+	const steps = [
+		'Account Information',
+		'Personal Details',
+		'Payment',
+		'Complete',
+	];
+
+	const diplayStep = (step) => {
 		switch (step) {
 			case 1:
 				return <Account />;
+			case 2:
+				return <Details />;
+			case 3:
+				return <Payment />;
+			case 4:
+				return <Final />;
 
 			default:
-				break;
 		}
+	};
+
+	const handleClick = (direction) => {
+		let newStep = currentStep;
+
+		direction === 'next' ? newStep++ : newStep--;
+
+		// check if step are within bounds
+		newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
 	};
 
 	return (
 		<div className='md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white'>
 			{/* Stepper */}
 			<div className='container horizontal mt-5'>
-				<Stepper />
+				<Stepper steps={steps} currentStep={currentStep} />
+
+				{/* Navigation Controls */}
+				<div className='my-10 p-10'>
+					<UseContextProvider
+					// value={{
+					// 	userData,
+					// 	setUserData,
+					// 	finalData,
+					// 	setFinalData,
+					// }}
+					>
+						{diplayStep(currentStep)}
+					</UseContextProvider>
+				</div>
 			</div>
 
 			{/* Navigation controls */}
-			<StepperControl />
+			{currentStep !== steps.length && (
+				<StepperControl
+					handleClick={handleClick}
+					currentStep={currentStep}
+					steps={steps}
+				/>
+			)}
 		</div>
 	);
 }
--- src/components/StepperControl.jsx
+++ src/components/StepperControl.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
-const StepperControl = () => {
+const StepperControl = ({ handleClick, currentStep, steps }) => {
 	return (
 		<div className='container flex justify-around mt-4 mb-8'>
 			{/* back button  */}
-			<button className='bg-white text-slate-400 uppercase py-2 px-4 rounded-xl font-samibold cursor-pointer border-2 border-slate-300 hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out'>
+			<button
+				onClick={() => handleClick()}
+				className={`bg-white text-slate-400 uppercase py-2 px-4 rounded-xl font-samibold cursor-pointer border-2 border-slate-300 hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out
+                ${currentStep === 1 ? 'opacity-50 cursor-not-allowed' : ''}
+                `}
+			>
 				Back
 			</button>
 			{/* next button */}
-			<button className='bg-green-500 text-white  uppercase py-2 px-4 rounded-xl font-samibold cursor-pointer  hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out'>
-				Next
+			<button
+				onClick={() => handleClick('next')}
+				className='bg-green-500 text-white  uppercase py-2 px-4 rounded-xl font-samibold cursor-pointer  hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out'
+			>
+				{currentStep === steps.length - 1 ? 'Confirm' : 'Next'}
 			</button>
 		</div>
 	);
